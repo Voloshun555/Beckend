c33b2cbb2372248642a0c3ad9c18ef766c97f757
Voloshun555 <Voloshunihor@gmail.com>
2024-01-31 19:05:19 +0200

    add  prisma and postgres

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { genSaltSync, hashSync } from 'bcryptjs';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  save(user: Partial<User>) {
+    const heshedPassword = this.hashPassword(user.password)
+    return this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: heshedPassword,
+        name: user.name,
+        roles: ['USER'],
+      },
+    });
+  }
+  findOne(idOrEmail: string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        OR: [
+          {
+            id: idOrEmail,
+          },
+          {
+            email: idOrEmail,
+          },
+        ],
+      },
+    });
+  }
+  delete(id: string) {
+    return this.prismaService.user.delete({
+      where: {id}
+    })
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10))
+  }
+}
