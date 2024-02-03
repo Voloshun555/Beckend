e2f024869e6a386c8db58711ea06ae524d4a931c
Voloshun555 <Voloshunihor@gmail.com>
2024-02-03 18:50:45 +0200

    add user-agent

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,45 +3,73 @@ import {
   Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  BadRequestException,
+  UnauthorizedException,
+  Res,
+  HttpStatus,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginDto } from './dto/login.dto';
+import { Tokens } from './interface';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { Cookie } from '@shared/decorators/cookies.decorator';
+import { UserAgent } from '@shared/decorators/user-agent.decorator';
+
+const REFRESH_TOKEN = 'refreshtoken'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService, private readonly configService: ConfigService) { }
 
   @Post('register')
-  register(@Body() registerDto: RegisterDto) {
-   
+  async register(@Body() registerDto: RegisterDto) {
+    const user = await this.authService.register(registerDto)
+
+    if (!user) {
+      throw new BadRequestException(`Не получається зарегіструвати користувача з даними ${JSON.stringify(registerDto)}`)
+    }
   }
   @Post('login')
-  login(@Body() loginDto: LoginDto) {
-   
+  async login(@Body() loginDto: LoginDto, @Res() res: Response, @UserAgent() agent: string) {
+    const tokens = await this.authService.login(loginDto, agent)
+
+    if (!tokens) {
+      throw new BadRequestException(`Не получається увійти з данами ${JSON.stringify(loginDto)}`)
+    }
+    this.setRefreshTokenCookies(tokens, res)
   }
 
-  @Get('refresh')
-  refreshTokens() {
-    return this.authService.findAll();
+  @Get('refresh-tokens')
+  async refreshTokens(@Cookie(REFRESH_TOKEN) refreshToken: string, @Res() res: Response, @UserAgent() agent: string) {
+    if (!refreshToken) {
+      throw new UnauthorizedException()
+    }
+
+
+    const tokens = await this.authService.refreshTokens(refreshToken, agent)
+
+    if (!tokens) {
+      throw new UnauthorizedException()
+    }
+    this.setRefreshTokenCookies(tokens, res)
+
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  private setRefreshTokenCookies(tokens: Tokens, res: Response) {
+    if (!tokens) {
+      throw new UnauthorizedException()
+    }
+    res.cookie(REFRESH_TOKEN, tokens.refreshToken.token, {
+      httpOnly: true,
+      sameSite: 'lax',
+      expires: new Date(tokens.refreshToken.exp),
+      secure: this.configService.get('NODE_ENV', 'development') === 'production',
+      path: '/',
+    })
+    res.status(HttpStatus.CREATED).json({ accessToken: tokens.accessToken })
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,116 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UsersService } from '@users/users.service';
+import { LoginDto } from './dto/login.dto';
+import { Tokens } from './interface';
+import { compareSync } from 'bcryptjs';
+import { Token, User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@prisma/prisma.service';
+import { v4 } from 'uuid';
+import { add } from 'date-fns';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) { }
-  
-  register(registerDto: RegisterDto) {
-    return 'This action adds a new auth';
+
+  private readonly logger = new Logger(AuthService.name)
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) { }
+
+  async refreshTokens(refreshToken: string, agent: string): Promise<Tokens> {
+    const token = await this.prismaService.token.findUnique({
+      where: {
+        token: refreshToken
+      }
+    })
+    if (!token) {
+      throw new UnauthorizedException()
+    }
+    await this.prismaService.token.findUnique({
+      where: {
+        token: refreshToken
+      }
+    })
+    if (new Date(token.exp) < new Date()) {
+      this.prismaService.token.delete({
+        where: {
+          token: refreshToken
+        }
+      })
+      throw new UnauthorizedException()
+    }
+
+    const user = await this.userService.findOne(token.userId)
+
+    return this.generateToken(user, agent)
+
   }
 
-  findAll() {
-    return `This action returns all auth`;
+
+  async register(registerDto: RegisterDto) {
+    const user: User = await this.userService.findOne(registerDto.email).catch((err) => {
+      this.logger.error(err)
+      return null
+    })
+
+    if (user) {
+      throw new ConflictException(`Користувач с таким ${registerDto.email} уже існує`)
+    }
+    return this.userService.save(registerDto).catch((err) => {
+      this.logger.error(err)
+      return null
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async login(loginDto: LoginDto, agent: string): Promise<Tokens> {
+    const user: User = await this.userService.findOne(loginDto.email).catch((err) => {
+      this.logger.error(err)
+      return null
+    })
+    if (!user || !compareSync(loginDto.password, user.password)) {
+      throw new UnauthorizedException('Не правельний логін чи пароль')
+    }
+
+    return this.generateToken(user, agent)
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  private async generateToken(user: User, agent: string): Promise<Tokens> {
+    const accessToken = 'Bearer ' + this.jwtService.sign({
+      id: user.id,
+      email: user.email,
+      roles: user.roles
+    })
+
+    const refreshToken = await this.getRefreshToken(user.id, agent)
+
+    return { accessToken, refreshToken }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private async getRefreshToken(userId: string, agent: string): Promise<Token> {
+    const _token = await this.prismaService.token.findFirst({
+      where: {
+        userId,
+        userAgent: agent
+      }
+
+    })
+    const token = _token?.token ?? '';
+    return this.prismaService.token.upsert({
+      where: { token },
+      update: {
+        token: v4(),
+        exp: add(new Date, { months: 1 }),
+      },
+      create: {
+        token: v4(),
+        exp: add(new Date, { months: 1 }),
+        userId,
+        userAgent: agent
+      }
+    })
   }
 }
