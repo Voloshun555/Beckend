c0f5b80920462b16308da4ec8b1e0840657f74fe
Voloshun555 <Voloshunihor@gmail.com>
2024-02-04 14:03:49 +0200

    add logout

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -113,4 +113,11 @@ export class AuthService {
       }
     })
   }
+
+  deleteRefreshToken(token: string) {
+    return this.prismaService.token.delete({
+      where: {
+      token
+    }})
+  }
 }
